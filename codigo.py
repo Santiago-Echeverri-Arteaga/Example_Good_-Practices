1b0f2ce6ef0f0e6bd2a6264ce7f65a0554018a23
Santiago Echeverri Arteaga <santiago513@gmail.com>
2023-01-31 18:35:45 -0500

    Update function codigo

--- codigo.py
+++ codigo.py
@@ -4,7 +4,18 @@ import numpy as np
 
 def main():
     print("Hello world!")
-    
-    
+
+
+def add(a: float, b: float):
+    """Suma de dos numeros dados a y b.
+
+    a: flotante
+    b: flotante
+
+    >>>add(1,2)
+    3"""
+    return a + b
+
+
 if __name__ == "__main__":
     main()
